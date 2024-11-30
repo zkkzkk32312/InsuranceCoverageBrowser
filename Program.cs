1bb17a577ebd4cdc16c43f3cf06fad354661eb74
Zack <zkkzkk32312@msn.com>
2024-11-30 14:41:08 -0700

    Use Blazor.Bootstrap for delete confirmation

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddBlazorBootstrap();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
